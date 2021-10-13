7f0cf48fdf274ba5c06f63d40967d492e6fbaa87
FylthyPlayz <jacob_cook_football@yahoo.com>
2021-10-13 14:24:36 -0500

    Initial Commit

---
+++ scripts/Employees.js
@@ -0,0 +1,42 @@
+import { getEmployees } from "./database.js"
+import { Products } from "./Products.js"
+import { getOrders } from "./database.js"
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("employee")) {
+            const [,employeeId] = itemClicked.id.split("--")
+
+            for (const employee of employees) {
+                if (employee.id === parseInt(employeeId)) {
+
+                    const employeeOrders = ordersGotten.filter(  // <--- Go to YouTube and search "javascript array filter"
+                    (order) => { 
+                        if (order.employeeId === employee.id) {
+                            return true
+                        }
+                    }
+                )
+                    window.alert(`${employee.name} sold ${employeeOrders.length} products`)
+                }
+            }
+        }
+    }
+)
+const employees = getEmployees()
+
+export const Employees = () => {
+    let html = "<ul>"
+
+    for (const employee of employees) {
+        html += `<li id="employee--${employee.id}">${employee.name}</li>`
+    }
+
+    html += "</ul>"
+
+    return html
+}
+
+const ordersGotten = getOrders()
